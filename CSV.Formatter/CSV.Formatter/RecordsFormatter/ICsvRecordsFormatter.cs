8c2d4e15eabbc84768916e3fa9b00edc1cb24045
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-04-22 15:58:41 +0300

    Create records formatter class

---
+++ CSV.Formatter/CSV.Formatter/RecordsFormatter/ICsvRecordsFormatter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CSV.Formatter
+{
+    public interface ICsvRecordsFormatter
+    {
+        void FormatEachRecord(IEnumerable<Timelog> records, string previousDate);
+        void BuildRecords(string currentDate, string previousDate, string[] inlineRecords);
+    }
+}
