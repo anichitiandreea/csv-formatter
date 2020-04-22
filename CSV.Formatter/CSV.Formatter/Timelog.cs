e43e48b4898bfe5aaae3b87cc137afa44895ee34
Andreea Anichiti <andreea.anichiti@gmail.com>
2020-04-22 21:54:16 +0300

    Add Time optional property to Timelog domain

--- CSV.Formatter/CSV.Formatter/Timelog.cs
+++ CSV.Formatter/CSV.Formatter/Timelog.cs
@@ -1,8 +1,13 @@
-﻿namespace CSV.Formatter
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace CSV.Formatter
 {
     public class Timelog
     {
         public string Date { get; set; }
         public string Comment { get; set; }
+
+        [Optional]
+        public string Time { get; set; }
     }
 }
